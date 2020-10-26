b2c3e88218e54dbf5b6c445d93147db05415e4af
Nick Kniola <nickkniola@me.com>
2020-10-26 16:54:53 -0700

    Listen for 'submit' events on the form.#1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 var avatarInput = document.querySelector('.avatar-input');
-var avatarImage = document.querySelector('.img-col img')
+var avatarImage = document.querySelector('.img-col img');
 
-avatarInput.addEventListener('input', function (event) {
+avatarInput.addEventListener('input', function () {
   avatarImage.setAttribute('src', avatarInput.value)
+});
+
+var form = document.querySelector('.profile-form');
+
+form.addEventListener('submit', function () {
+
 })
